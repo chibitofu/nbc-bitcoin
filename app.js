bca2d04f7dfacfec1b9d4d8fdec75eb9efe25b86
chibitofu <erinmoony@gmail.com>
2020-02-10 16:03:48 -0800

    Basic view working.

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+})
+
 module.exports = app;
+
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  fs.readFile('public/data.json', (err, data) => {
+    if (err) throw err;
+    data = JSON.parse(data);
+    data = data.data.history;
+    console.log(data)
+    res.render('index', { data });
+  })
+  
 });
 
 module.exports = router;
