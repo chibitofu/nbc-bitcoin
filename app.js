96363f7b7961742d766ea68d7183682dc0503b75
chibitofu <erinmoony@gmail.com>
2020-02-11 14:53:43 -0800

    Cleaned up depricated files.

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,7 +13,6 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,1 +1,1 @@
-console.log("Script works")
+
